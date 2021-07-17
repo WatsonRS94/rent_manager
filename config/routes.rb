e8f0fe3615e169acc4cb9982df9a2a2881e2d472
WatsonRs94 <watson.rs94@gmail.com>
2021-07-17 19:20:19 -0300

    pre user.photo

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   namespace :admin do
     get '/dashboard', to: 'dashboard#index'
     resources :users
+    #do
+    # member do get :edit_photo; post :edit_photo; delete :destroy_photo; end
+    #end
     post '/authenticate', to: 'login#authenticate'
     delete '/logout', to: 'login#logout'
     get '/', to: 'login#index'
